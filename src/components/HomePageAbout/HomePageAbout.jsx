45777eb1b7885041d65adc69fb0bcc1151d9ae29
Suzanne Aitchison <suzanne.aitchison@gmail.com>
2020-01-26 20:53:24 +0000

    add basic category tiles in desktop sizing

--- src/components/HomePageAbout/HomePageAbout.jsx
+++ src/components/HomePageAbout/HomePageAbout.jsx
@@ -6,19 +6,11 @@ class HomePageAbout extends React.Component {
   render() {
     return (
       <div className={aboutStyle.aboutWrapper}>
-        <h1>A web accessibility toolkit with a React focus</h1>
+        <h1>web accessibility toolkit</h1>
         <p>
-          Welcome to Up Your A11y! This site aims to be a resource for front-end
-          developers to find useful information on how to make your sites more
-          accessible. The topics covered have a React bias, but the principles
-          in each apply to all web development, so please don't be put off if
-          you don't work with React specifically!
+          tutorials &amp; resources for developers looking to build accessible web experiences
         </p>
-        <p>
-          Articles are organised into sections below, and please don't hesitate
-          to <Link to="/contact">Contact Me</Link> with suggestions for current
-          and future content.
-        </p>
+       
       </div>
     )
   }

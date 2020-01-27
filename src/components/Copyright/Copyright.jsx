b68ce170242377957f9c6ae74d10111b0b9626e1
Suzanne Aitchison <suzanne.aitchison@gmail.com>
2020-01-27 22:03:50 +0000

    fix alignment

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -4,7 +4,9 @@ import copyrightStyle from './copyright.module.css'
 export default function Copyright() {
   return (
     <footer className={copyrightStyle.footer}>
-      <p>&copy; Suzanne Aitchison 2020</p>
+      <div className={copyrightStyle.innerContainer}>
+        <p>&copy; Suzanne Aitchison 2020</p>
+      </div>
     </footer>
   )
 }

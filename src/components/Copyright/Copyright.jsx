2ec3551452cf3e45574d485222a59b5f337ace52
Suzanne Aitchison <suzanne.aitchison@gmail.com>
2020-01-27 21:59:29 +0000

    copyright notice

---
+++ src/components/Copyright/Copyright.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import copyrightStyle from './copyright.module.css'
+
+export default function Copyright() {
+  return (
+    <footer className={copyrightStyle.footer}>
+      <p>&copy; Suzanne Aitchison 2020</p>
+    </footer>
+  )
+}

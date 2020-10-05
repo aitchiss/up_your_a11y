1f34a4f1dd5f4f6e10245ef494e1d7193df61b3a
Monica Mateiu <monica.mateiu@gmail.com>
2020-10-05 12:47:32 +0100

    refactor functions into arrow functions

--- src/components/ErrorForm/ErrorFormInlineError.jsx
+++ src/components/ErrorForm/ErrorFormInlineError.jsx
@@ -1,77 +1,57 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import formStyle from './errorForm.module.css'
 
-class ErrorFormInlineError extends React.Component {
-  emailRef = React.createRef()
+export default function ErrorFormInlineError() {
+  const emailRef = useRef(null)
+  const [emailInputValid, setEmailInputValid] = useState()
 
-  constructor(props) {
-    super(props)
+  const validateEmail = value => value !== '';
 
-    this.state = {
-      emailInputValid: undefined,
-    }
-
-    this.validateEmail = this.validateEmail.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleBlur = this.handleBlur.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+  const handleChange = () => {
+    setEmailInputValid(true)
   }
 
-  validateEmail(value) {
-    return value !== ''
+  const handleBlur = () => {
+    const { value } = emailRef.current
+    const isEmailInputValid = validateEmail(value)
+    setEmailInputValid(isEmailInputValid)
   }
 
-  handleChange() {
-    this.setState({ emailInputValid: true })
-  }
-
-  handleBlur() {
-    const value = this.emailRef.current.value
-    const emailInputValid = this.validateEmail(value)
-
-    this.setState({ emailInputValid })
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = e => {
     e.preventDefault()
-    const value = this.emailRef.current.value
-    if (!this.validateEmail(value)) {
-      this.setState({ emailInputValid: false })
+    const { value } = emailRef.current
+    if (!validateEmail(value)) {
+      setEmailInputValid(false)
       return
     }
     // Handle any actions for valid form submissions
   }
 
-  render() {
-    const { emailInputValid } = this.state
-    const errorMessage =
-      emailInputValid === false ? 'Email should not be blank' : ''
+  const errorMessage =
+    emailInputValid === false ? 'Email should not be blank' : ''
 
-    return (
-      <form>
-        <label htmlFor="email-inline">Email</label>
-        <input
-          ref={this.emailRef}
-          id="email-inline"
-          aria-invalid={emailInputValid === false ? true : null}
-          onBlur={this.handleBlur}
-          onChange={this.handleChange}
-          className={formStyle.formInput}
-        />
-        <div role="alert">
-          <span className={formStyle.errorText}>{errorMessage}</span>
-        </div>
-        <button
-          type="submit"
-          className={formStyle.formButton}
-          onClick={this.handleSubmit}
-          aria-disabled={emailInputValid === true ? null : true}
-        >
-          Submit
-        </button>
-      </form>
-    )
-  }
+  return (
+    <form>
+      <label htmlFor="email-inline">Email</label>
+      <input
+        ref={emailRef}
+        id="email-inline"
+        aria-invalid={emailInputValid === false ? true : null}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        className={formStyle.formInput}
+      />
+      <div role="alert">
+        <span className={formStyle.errorText}>{errorMessage}</span>
+      </div>
+      <button
+        type="submit"
+        className={formStyle.formButton}
+        onClick={handleSubmit}
+        aria-disabled={emailInputValid === true ? null : true}
+      >
+        Submit
+      </button>
+    </form>
+  )
 }
-
-export default ErrorFormInlineError
--- src/components/ErrorForm/ErrorFormWithList.jsx
+++ src/components/ErrorForm/ErrorFormWithList.jsx
@@ -1,62 +1,49 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import formStyle from './errorForm.module.css'
 
-class ErrorFormWithList extends React.Component {
-  emailRef = React.createRef()
+export default function ErrorFormWithList() {
+  const emailRef = useRef(null)
+  const [emailInputValid, setEmailInputValid] = useState()
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      emailInputValid: undefined,
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleSubmit(e) {
-    const value = this.emailRef.current.value
-    const emailInputValid = value !== ''
-    this.setState({ emailInputValid })
+  const handleSubmit = e => {
+    const { value } = emailRef.current
+    const isEmailInputValid = value !== ''
+    setEmailInputValid(isEmailInputValid)
     // Handle submission of form if all fields valid
     e.preventDefault()
   }
 
-  render() {
-    const { emailInputValid } = this.state
-    const errorMessage = emailInputValid ? '' : 'Email should not be blank'
-    return (
-      <React.Fragment>
-        {emailInputValid === false ? (
-          <div role="alert">
-            <p className={formStyle.errorListHeader}>
-              Looks like there are some errors in your form:
-            </p>
-            <ul>
-              <li>{errorMessage}</li>
-            </ul>
-          </div>
-        ) : null}
+  const errorMessage = emailInputValid ? '' : 'Email should not be blank'
+  return (
+    <React.Fragment>
+      {emailInputValid === false ? (
+        <div role="alert">
+          <p className={formStyle.errorListHeader}>
+            Looks like there are some errors in your form:
+          </p>
+          <ul>
+            <li>{errorMessage}</li>
+          </ul>
+        </div>
+      ) : null}
 
-        <form>
-          <label htmlFor="email-with-list">Email</label>
-          <input
-            ref={this.emailRef}
-            id="email-with-list"
-            className={formStyle.formInput}
-            required
-            aria-invalid={emailInputValid === false ? true : null}
-          />
-          <button
-            type="submit"
-            className={formStyle.formButton}
-            onClick={this.handleSubmit}
-          >
-            Submit
-          </button>
-        </form>
-      </React.Fragment>
-    )
-  }
+      <form>
+        <label htmlFor="email-with-list">Email</label>
+        <input
+          ref={emailRef}
+          id="email-with-list"
+          className={formStyle.formInput}
+          required
+          aria-invalid={emailInputValid === false ? true : null}
+        />
+        <button
+          type="submit"
+          className={formStyle.formButton}
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </form>
+    </React.Fragment>
+  )
 }
-
-export default ErrorFormWithList
